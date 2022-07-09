ee560dfd44ec1171637b9073998465621c15cf10
Iago Cavalcante <iagoangelimc@gmail.com>
2022-07-08 22:35:36 -0300

    feat: configure tailwind inside project

--- backend/config/dev.exs
+++ backend/config/dev.exs
@@ -26,7 +26,8 @@ config :gym_api, GymApiWeb.Endpoint,
   secret_key_base: "YKUpDmY9vijql/r2mi49I00De/fRoYbdw4BrZC8PGglvymjV8he3Y169zWX5NbTC",
   watchers: [
     # Start the esbuild watcher by calling Esbuild.install_and_run(:default, args)
-    esbuild: {Esbuild, :install_and_run, [:default, ~w(--sourcemap=inline --watch)]}
+    esbuild: {Esbuild, :install_and_run, [:default, ~w(--sourcemap=inline --watch)]},
+    tailwind: {Tailwind, :install_and_run, [:default, ~w(--watch)]}
   ]
 
 # ## SSL Support
